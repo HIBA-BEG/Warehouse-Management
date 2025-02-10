5492ad9da4792d29fd79dee90608e06fe4a81f06
HIBA BEGHDI <beghiba@gmail.com>
2025-02-11 00:45:01 +0100

    WM-34 the user can log in and be redirected to the products page

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
+import { loginWarehouseman } from '../services/api';
+import { router } from 'expo-router';
+
+const Login: React.FC = () => {
+    const [secretKey, setSecretKey] = useState('');
+
+    const handleLogin = async () => {
+        const result = await loginWarehouseman(secretKey);
+    
+        if (result.success) {
+          console.log('Login successful:', result.warehouseman);
+          router.push('/(tabs)/products');
+        } else {
+          Alert.alert('Login Failed', result.error || 'Invalid secret key. Please try again.');
+        }
+      };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter you secretKey"
+                value={secretKey}
+                onChangeText={setSecretKey}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#171717',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    input: {
+        width: '100%',
+        color: '#fff',
+        fontWeight: 'bold',
+        height: 50,
+        boxShadow: 'inset 2px 5px 10px rgb(5, 5, 5)',
+        borderWidth: 1,
+        borderRadius: 15,
+        marginBottom: 15,
+        paddingHorizontal: 25,
+    },
+    forgotPassword: {
+        marginTop: 15,
+        color: 'blue',
+    },
+    button: {
+        backgroundColor: '#252525',
+        boxShadow: 'inset 2px 5px 10px rgb(5, 5, 5)',
+        borderRadius: 15,
+        borderColor: '#ccc',
+        borderWidth: 2,
+        padding: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+    },
+});
+
+export default Login;
